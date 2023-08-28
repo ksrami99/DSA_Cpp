860b14aef13e18b99ff18d6927b2b9662fda1ddf
Karan <ksrami99@gmail.com>
2023-08-28 21:19:29 +0530

    else if Condition

--- Week 1/if_else.cpp
+++ Week 1/if_else.cpp
@@ -11,14 +11,28 @@ int main()
     // }
 
 
+    // int sides;
+    // cout<<"How much sides does it have ?"<<endl;
+    // cin>>sides;
+    // if (sides == 3)
+    // {
+    //     cout<<"It's Triangle"<<endl;
+    // }else{
+    //     cout<<"It's not triangle"<<endl;
+    // }
+
     int sides;
     cout<<"How much sides does it have ?"<<endl;
     cin>>sides;
     if (sides == 3)
     {
         cout<<"It's Triangle"<<endl;
+    }else if(sides == 4){
+        cout<<"It's not Square"<<endl;
+    }else if(sides == 6){
+        cout<< "It's Hexagon"<<endl;
     }else{
-        cout<<"It's not triangle"<<endl;
+        cout<<"To fir muje nai pata "<<endl;
     }
     
     return 0;

ac96659f77916cb08a98427b4089c8d2705d077d
Karan <ksrami99@gmail.com>
2023-08-28 21:16:32 +0530

    If else Condition

--- Week 1/if_else.cpp
+++ Week 1/if_else.cpp
@@ -3,11 +3,23 @@ using namespace std;
 
 int main()
 {
-    int balance;
-    cout<<"How much balance You have ?"<<endl;
-    cin>>balance;
-    if(balance>=10){
-        cout<<"You can buy maggie"<<endl;
+    // int balance;
+    // cout<<"How much balance You have ?"<<endl;
+    // cin>>balance;
+    // if(balance>=10){
+    //     cout<<"You can buy maggie"<<endl;
+    // }
+
+
+    int sides;
+    cout<<"How much sides does it have ?"<<endl;
+    cin>>sides;
+    if (sides == 3)
+    {
+        cout<<"It's Triangle"<<endl;
+    }else{
+        cout<<"It's not triangle"<<endl;
     }
+    
     return 0;
 }

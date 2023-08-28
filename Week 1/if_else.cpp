213946fb68151cc453fa36fb7f5602c24f516958
Karan <ksrami99@gmail.com>
2023-08-28 21:12:35 +0530

    If Condition

---
+++ Week 1/if_else.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int balance;
+    cout<<"How much balance You have ?"<<endl;
+    cin>>balance;
+    if(balance>=10){
+        cout<<"You can buy maggie"<<endl;
+    }
+    return 0;
+}

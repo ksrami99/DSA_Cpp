729da0c887dcfbecbcd7d0760dea04db1535a156
Karan <ksrami99@gmail.com>
2023-08-28 21:42:09 +0530

    Intro to Loops

---
+++ Week 1/loop.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+// Loops
+//      For loop
+//      while loop
+//      Do-While loop
+//      for-each loop
+
+int main()
+{
+    // Fore Loop
+    for( int i = 0 ;        i < 10 ;        i++ )
+    //   initialization     Condition       Updation
+    {
+        cout<<"Karan"<<endl; 
+    }
+    return 0;
+}
+
+

6cff4062a0ddf1effef64f5f2534b8a70e47d209
Karan <ksrami99@gmail.com>
2023-08-28 22:14:00 +0530

    More on loops

--- Week 1/loop.cpp
+++ Week 1/loop.cpp
@@ -9,12 +9,38 @@ using namespace std;
 
 int main()
 {
-    // Fore Loop
-    for( int i = 0 ;        i < 10 ;        i++ )
-    //   initialization     Condition       Updation
-    {
-        cout<<"Karan"<<endl; 
+    // // Fore Loop
+    //      Not Mendetory       Not Mendetory   Not Mendetoryzx
+    // for( int i = 0 ;         i < 10 ;        i++ )
+    // //   initialization      Condition       Updation
+    // {
+    //     cout<<"Karan"<<endl; 
+    // }
+
+    // Counting
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     cout<<i<<endl;
+    // }
+
+    // Reverse Counting
+    // for (int i = 10; i >=1; i--)
+    // {
+    //     cout<<i<<endl;
+    // }
+
+    // Nested Loop
+    for(int i =0;i<5;i++){
+        cout<<"Outer Loop "<< i << endl;
+
+        // for each outer loop Inner loop will run saperatly 
+        // In this case inner loop will run 3 timed for each outer loop
+        for(int j = 0;j<3;j++){
+            cout<< "Inner Loop "<<j<<endl;
+        }
     }
+    
+    
     return 0;
 }
 
